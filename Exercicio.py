46da9ccce4fd46f761b6b03c794ce3fea550120b
Thamyres0 <thamyresr223.viana@gmail.com>
2022-10-25 21:31:24 -0300

    Arquivo com variaveis a,b,c,d

---
+++ Exercicio.py
@@ -0,0 +1,15 @@
+a = 10
+b = 10.5
+c = "thamyres"
+d = True
+
+print("O valor da variavel a é: " + str(a) + " e o tipo dela é ")
+print(type(a))
+print("o valor da variavel b é: " + str(a) + " e o tipo dela é ")
+print(type(b))
+print("o valor da variavel c é: " + str(a) + " e o tipo dela é ")
+print(type(c))
+print("o valor da variavel d e: " + str(a) + " e o tipo dela é ")
+print(type(d))
+
+

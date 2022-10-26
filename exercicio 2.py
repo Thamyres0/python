d641119c180fc96b939a35487c9c3e13ddd708d8
Thamyres0 <thamyresr223.viana@gmail.com>
2022-10-25 21:54:55 -0300

    Arquivo com valores input, int, float

---
+++ exercicio 2.py
@@ -0,0 +1,14 @@
+a = 1
+b = 1
+c = 1
+
+input("o valor da variavel é: " + str(a) + "um")
+print(type(a))
+int("o valor da variavel a é: " + str(b) + "1")
+print(type(a))
+float("o valor da variavel é: + str(c) + 1.0")
+print(type(a))
+
+
+
+

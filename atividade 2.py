b7981543f0b43e9a57a627f9c3ecec9a81815e13
Thamyres0 <thamyresr223.viana@gmail.com>
2022-10-26 21:51:17 -0300

    atividade 2

---
+++ atividade 2.py
@@ -0,0 +1,41 @@
+#algoritimo para receber valores de 5 produtos
+
+Iphone = 2980
+Tablet = 2540
+PS5 = 1950
+Notebook = 2100
+Samsung = 2540
+
+
+input("digite um produto: str (Iphone 2980) ")
+print("quantos Iphones você quer comprar? ")
+input("digite um produto: str (Tablet 2540) ")
+print("quantos Tablets você quer comprar? ")
+input("digite um produto: str (PS5 1950) ")
+print("quantos PS5 você quer comprar? ")
+input("digite um produto: str (Notbook 2100) ")
+print("quantos Notbooks você quer comprar? ")
+input("digite um produto: str (Samsung 2540) ")
+print("quantos Sansung você quer comprar? ")
+input("o total da sua compra foi: ")
+Total = ("quantidade * Iphone" "quantidade * Tablet" "quantidade * PS5" "quantidade * Notebook" "quantidade * samsung")
+input ("O valor total da sua compra parcelada em 3 vezes é: str (total/3")
+print("parcela = total/3")
+input("O valor total da sua compra parcelada em 6 vezes com 5% de acrescimo é: str (total*1.5/6)")
+print("parcela = total*1.5/6")
+input("O valor total da sua compra com 10% de desconto é: str (total-10.0")
+print("total = total-10.0")
+input("se não deseja comprar nenhum produto digite 0")
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ exercicio 2 corrigido.py
@@ -0,0 +1,9 @@
+# atividade receber dois valores um texto e um inteiro e mostrar no final
+
+caractere = input("digite uma palavra: ")
+
+inteiro = input("digite um numero inteiro: ")
+
+print("A variavel é do tipo: ", type(caractere))
+print("O texto digitado foi; ", caractere)
+print("A variavel e´do tipo: ", type(inteiro))

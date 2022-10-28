51c742bd67c3fc277fbaf4d7a7b29abba16a020b
Thamyres0 <thamyresr223.viana@gmail.com>
2022-10-27 21:48:32 -0300

    tabuada

---
+++ Atividadetabuada.py
@@ -0,0 +1,14 @@
+#crie um programa para realizar operações
+
+tabuada = input ("digite um numero para saber a tabutada de 1 a 10: ")
+inicio = input("digite o inicio do intervalo da tabuada: ")
+fim = input("digite o fim do intervalo da tabuada: ")
+
+
+
+
+
+
+
+
+

1af6a8eb4b8166399e285a2d33b1e9358ca0f02c
Thamyres0 <thamyresr223.viana@gmail.com>
2022-11-01 19:57:24 -0300

    atividadeenem

---
+++ atividade 2 continuação.py
@@ -0,0 +1,6 @@
+# programa que leia um conjunto de nomes de 20 estudantes inscritos na prova do ENEM.
+
+Lista = ["Thamyres", "Gabriel", "Layane", "Gustavo", "Ronei", "Vitor", "Gilberto", "Artur", "Camila", "Leticia", "Hudson", "Larissa", "Rodrigo", "Marcos", "Talita", "Lucas", "Kayo", "Marcela", "Roberto", "Tais"]
+
+print (Lista)
+print (sorted (Lista))

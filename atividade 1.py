f6e84a7f0d392d35eaa7fa2b8ed5c30db03ef2ce
Thamyres0 <thamyresr223.viana@gmail.com>
2022-10-31 21:36:25 -0300

    atividadeconsoantes

---
+++ atividade 1.py
@@ -0,0 +1,30 @@
+#Faça um Programa que leia em um vetor de 10 caracteres (letras ) e diga quantas consoantes foram lidas.
+
+lista = [0,0,0,0,0,0,0,0,0,0]
+
+for i in range(10):
+    lista[i] = str(input ("digite uma letra para cada posição: "))
+vogal = input ("a", "e", "i", "o", "u" )
+
+Lista = str(input("digite uma letra"))
+encontrado = False
+
+for i in range(10):
+    if(Lista[i] == Lista):
+        print("letra encontrada: ",Lista, "na posição", Lista[i])
+        encontrado = True
+    if(encontrado is False):
+        print("nao fo encontrado na busca sua lista")
+    print[Lista]
+
+
+
+
+
+
+
+
+
+
+
+

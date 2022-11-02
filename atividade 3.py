026e38213cc3e4a409b723e101cb156e76e65abd
Thamyres0 <thamyresr223.viana@gmail.com>
2022-11-01 21:21:59 -0300

    atividademultiplicaçao

---
+++ atividade 3.py
@@ -0,0 +1,9 @@
+def sum(num1, num2):
+    return num1 * num2
+print("\tmultiplicaçaõ de dois numeros\n")
+
+num1 = int(input("digite um numero: "))
+num2 = int(input("digite mais um numero: "))
+
+print("")
+print("o resultado da multiplicação e %d" % sum(num1 , num2))
